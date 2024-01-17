709239f625b72ae033dd011d9c5f6e702edb5d09
Kayanski <kowalski.kowalskin@gmail.com>
2024-01-17 14:36:13 +0100

    Public query module

--- smart-contracts/contracts/cl-vault/src/vault/admin.rs
+++ smart-contracts/contracts/cl-vault/src/vault/admin.rs
@@ -143,8 +143,7 @@ mod tests {
         let mut deps = mock_dependencies();
 
         build_tick_exp_cache(&mut deps.storage).unwrap();
-        let verify_resp = verify_tick_exp_cache(&mut deps.storage).unwrap();
-        assert_eq!((), verify_resp);
+        verify_tick_exp_cache(&mut deps.storage).unwrap();
     }
 
     #[test]
